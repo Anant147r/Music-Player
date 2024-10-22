d5d929ada973eae3fe9c6d8e796857a99e7f1ea0
Anant Rawat <anant1995rawat1995@gmail.com>
2024-10-22 16:50:58 +0530

    feat : set basic layout

---
+++ src/components/MusicPlayer.tsx
@@ -0,0 +1,13 @@
+const MusicPlayer = () => {
+  return (
+    <div className="p-10 bg-gray-800 rounded-xl shadow-xl w-80">
+      {/* Top Header Section */}
+      <div className="w-full flex items-center justify-between mb-10">
+        <div className="">-</div>
+        <p>Playing Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default MusicPlayer;
