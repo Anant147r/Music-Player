b9a6639e0e9b91d7b11b95d09e08150836c026df
Anant Rawat <anant1995rawat1995@gmail.com>
2024-10-28 11:57:08 +0530

    feat : set up UI layout for the music player

---
+++ src/components/SongDetails.tsx
@@ -0,0 +1,26 @@
+const SongDetails = () => {
+  return (
+    <div className="flex flex-col items-center">
+      {/* Spinning Disk */}
+      <div className="relative w-36 h-36 rounded-full">
+        {/* Disk Image with rotation style */}
+        <img
+          src="images/Austin-Mahone.jpg"
+          alt=""
+          className="absolute w-full h-full object-cover object-center z-20 rounded-full"
+        />
+        {/* Glowing ring around the disc */}
+
+        <div className="absolute top-[-5%] left-[-5%] rounded-full glow-ring w-[110%] h-[110%] bg-gradient-to-r from-green-300 to-blue-400 opacity-30"></div>
+      </div>
+
+      {/* song title & artist */}
+      <div className="text-center my-5">
+        <h2 className="text-xl font-bold text-white">Dancing in the dark </h2>
+        <p className="text-gray-400">Rihana</p>
+      </div>
+    </div>
+  );
+};
+
+export default SongDetails;
